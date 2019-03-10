fa37fed58ba9627e4bd0ae6e34bbbceaf0df746f
antony-jr <antonyjr@protonmail.com>
2019-03-10 22:17:48 +0530

    add towers of hanoi

---
+++ experiment-12.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void towerOfHanoi(int n , char src , char dest , char spare){
+	if(!n)
+		return;
+	towerOfHanoi(n - 1 , src , spare , dest);
+	printf("Move disk %d from peg %c to peg %c.\n" , n , src , dest);
+	towerOfHanoi(n - 1 , spare , dest , src);
+}
+void main(){
+	int n;
+	printf("Enter the number of disk(s): ");
+	scanf("%d" , &n);
+	towerOfHanoi(n , 'A' , 'C' , 'B');
+}
